2c70072333c6456d2b6563e7893d02324637b0f1
helciodev <helcioandre@live.com>
2021-05-17 15:41:12 +0100

    Add eventListener for btnNew and formParent variables

--- main.js
+++ main.js
@@ -9,3 +9,13 @@ const booksWrapper = document.querySelector("#books")
 let books = []
 let book;
 let stored = ''
+
+btnNew.addEventListener('click', function() {
+  formParent.classList.toggle('hidden')
+  form.classList.add('get-down')
+})
+
+formParent.addEventListener('click', function() {
+this.classList.toggle('hidden')
+form.classList.toggle('get-down')
+})
