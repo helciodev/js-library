08adf78a39765bc1d2ca4f2a61c8ccc1ff84be21
helciodev <helcioandre@live.com>
2021-05-17 15:44:24 +0100

    Create function AddBookToLibrary

--- main.js
+++ main.js
@@ -26,3 +26,20 @@ function makeBook(title, author, pages, description){
    this.pages = pages
    this.description = description
  }
+
+ function addBookToLibrary(e) {
+  e.preventDefault()
+  book = new makeBook(
+    inputTitle.value,
+    inputAuthor.value,
+    inputPages.value,
+    inputDescription.value
+  )
+
+  books.push(book)
+   form.reset()
+   this.classList.toggle('get-down')
+   formParent.classList.toggle('hidden')
+   displayBook(book)
+  
+}
